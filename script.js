02963663a6e26d44e0e5e8055258a8275fbf77d4
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-22 13:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+
+output.innerHTML = `
+  <tr>
+    <td colspan="2" class="text-center">Loading...</td>
+  </tr>
+`;
+
+function createPromise(name) {
+  const time = Math.floor(Math.random() * 3) + 1;
+  const ms = time * 1000;
+
+  return new Promise(resolve => {
+    setTimeout(() => resolve({ name, time }), ms);
+  });
+}
+
+const start = performance.now();
+
+const promises = [
+  createPromise("Promise 1"),
+  createPromise("Promise 2"),
+  createPromise("Promise 3"),
+];
+
+Promise.all(promises).then(results => {
+  const totalTime = (performance.now() - start) / 1000;
+
+  output.innerHTML = "";
+
+  results.forEach(p => {
+    output.innerHTML += `
+      <tr>
+        <td>${p.name}</td>
+        <td>${p.time}</td>
+      </tr>
+    `;
+  });
+
+  output.innerHTML += `
+    <tr class="table-info fw-bold">
+      <td>Total</td>
+      <td>${totalTime.toFixed(3)}</td>
+    </tr>
+  `;
+});
